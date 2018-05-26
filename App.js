3176cbdca15613317df76455b31c6af5338b3acd
andrwsalcdo <asalcedo07@gmail.com>
2018-05-26 12:21:17 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import RootNavigator from './RootNavigator';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
+};
+
+export default App;
