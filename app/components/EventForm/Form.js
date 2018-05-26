9c603d791c98043a378b2cc081315da8ed35b5f0
Agnieszka Jankowska <ajankowska0404@gmail.com>
2018-05-26 16:18:40 +0200

    basic event form component

--- app/components/EventForm/Form.js
+++ app/components/EventForm/Form.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import  * as React from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
-const Form = () => (
-  <View>
-    <Text> this is the form</Text>
-  </View>
-);
 
-export default Form;
+class EventForm extends React.Component {
+    state = {
+        name: '',
+        time: '',
+        description: '',
+        isDateTimePickerVisible: false
+    }
+
+    _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+    _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+    _handleDatePicked = (date) => {
+        console.log('A date has been picked: ', date);
+        this._hideDateTimePicker();
+    };
+
+
+    render() {
+        console.log('state', this.state.name, this.state.description, this.state.time)
+        return (
+            <View>
+                <Text>Name</Text>
+                <TextInput onChangeText={(name) => this.setState({name: name})}/>
+                <View style={{ width: 300 }}>
+                    <TouchableOpacity onPress={this._showDateTimePicker}>
+                        <Text>Show DatePicker</Text>
+                    </TouchableOpacity>
+                    <DateTimePicker
+                        isVisible={this.state.isDateTimePickerVisible}
+                        onConfirm={this._handleDatePicked}
+                        onCancel={this._hideDateTimePicker}
+                        mode={'time'}
+                    />
+                </View>
+                <Text>Description</Text>
+                <TextInput onChangeText={(description) => this.setState({description: description})}/>
+            </View>
+        )
+    }
+}
+
+export default EventForm;
