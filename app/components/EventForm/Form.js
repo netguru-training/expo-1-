4ef1c3714469a2ef24d12a077ede992d3ad5228a
Agnieszka Jankowska <ajankowska0404@gmail.com>
2018-05-26 15:26:36 +0200

    event form

--- app/components/EventForm/Form.js
+++ app/components/EventForm/Form.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import TimePicker from 'react-native-time-picker';
 
-const Form = () => (
-  <View>
-    <Text> this is the form</Text>
-  </View>
-);
+class EventForm extends React.Component {
+   state = {
+       name: '',
+       time: '',
+       description: ''
+   }
 
-export default Form;
+
+
+    render() {
+       console.log('state', this.state.name, this.state.description, this.state.time)
+        return (
+            <View>
+                <Text>Name</Text>
+                <TextInput onChangeText={(name) => this.setState({name: name})}/>
+                <Text>Time</Text>
+                <TextInput />
+                <Text>Description</Text>
+                <TextInput onChangeText={(description) => this.setState({description: description})}/>
+                <TimePicker
+                    selectedHour={0}
+                    selectedMinute={30}
+                    minuteInterval={5}
+                    loop={true} />
+            </View>
+        )
+    }
+}
+
+export default EventForm;
